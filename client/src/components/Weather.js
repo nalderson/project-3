7d24870415c227237cfa2a07bcc5ca663ed3f81c
Tash Alderson <aldersonnatasha@gmail.com>
2021-02-26 14:20:59 +0000

    deployment

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -11,7 +11,7 @@ export default function Weather({ city }) {
   useEffect(() => {
     async function getData() {
       try {
-        const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=${process.env.weatherAPI}`)
+        const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=${process.env.weatherAPI}`)
         updateWeathers(data.list[0])
         updateLoading(false)
       } catch (err) {
@@ -25,7 +25,7 @@ export default function Weather({ city }) {
   }
   return <div>
     <div className="main-weather">
-      <h2 className="title is-2" id="weather-title">   {Math.round(weathers.main.temp)}°C</h2>      <img src={`http://openweathermap.org/img/w/${weathers.weather[0].icon}.png`} id="weather-picture" alt='picture of the weather' width="80" />
+      <h2 className="title is-2" id="weather-title">   {Math.round(weathers.main.temp)}°C</h2>      <img src={`https://openweathermap.org/img/w/${weathers.weather[0].icon}.png`} id="weather-picture" alt='picture of the weather' width="80" />
     </div>
     <div>Feels like {Math.round(weathers.main.feels_like)} °C. {weathers.weather.main}</div>
     <div>{weathers.weather.icon}</div>
